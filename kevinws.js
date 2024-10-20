1a860f16094dde66b160512d25ba7428f215c38c
Kevin-Pro-Code <kevinprofessionalman@outlook.com>
2024-10-20 20:52:18 -0300

    .

--- kevinws.js
+++ kevinws.js
@@ -502,9 +502,27 @@ alert("This website is under construction!!! Check the projects and other tabs t
 
 */
 
+//SET BROWSER TO WORK WITH ON MOBILE: CALL CSS chrome-mobile body {} FOR EXAMPLE
 
 document.addEventListener('DOMContentLoaded', function() {
-    if (navigator.userAgent.toLowerCase().indexOf('chrome') > -1 && /Mobi|Android/i.test(navigator.userAgent)) {
-        document.body.classList.add('chrome-mobile');
+    const userAgent = navigator.userAgent.toLowerCase();
+    const isMobile = /Mobi|Android/i.test(navigator.userAgent);
+
+    if (isMobile) {
+        if (userAgent.indexOf('chrome') > -1) {
+            document.body.classList.add('chrome-mobile');
+        }
+        
+        if (userAgent.indexOf('opera') > -1 || userAgent.indexOf('opr') > -1) {
+            document.body.classList.add('opera-mobile');
+        }
+        
+        if (userAgent.indexOf('edg') > -1) {
+            document.body.classList.add('edge-mobile');
+        }
+        
+        if (userAgent.indexOf('firefox') > -1) {
+            document.body.classList.add('firefox-mobile');
+        }
     }
 });
